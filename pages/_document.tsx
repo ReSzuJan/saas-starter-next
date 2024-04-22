28de5f37ff3e09717e3109ec8ff086084d001500
SzuJanLin <szujan.lin@gmail.com>
2024-04-22 16:10:36 -0600

    add harmony

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document';
-
+import { HarmonySetup } from 'harmony-ai-editor';
 export default function Document() {
   return (
     <Html lang="en" className="h-full" data-theme="boxyhq">
@@ -7,6 +7,7 @@ export default function Document() {
       <body className="h-full">
         <Main />
         <NextScript />
+        {process.env.NODE_ENV !== 'production' ? <HarmonySetup repositoryId="f7e8579e-6e20-40c7-9202-4bb8031c3036" /> : null}
       </body>
     </Html>
   );
