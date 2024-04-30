7c2cd7feed4ddcb22931651e45ea8ed471dff303
SzuJanLin <szujan.lin@gmail.com>
2024-04-30 00:56:54 -0600

    change button color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,11 @@
 import { Head, Html, Main, NextScript } from 'next/document';
-import { HarmonySetup } from 'harmony-ai-editor';
 export default function Document() {
   return (
     <Html lang="en" className="h-full" data-theme="boxyhq">
       <Head />
       <body className="h-full">
         <Main />
-        <NextScript />
-        {process.env.NODE_ENV !== 'production' ? <HarmonySetup repositoryId="f7e8579e-6e20-40c7-9202-4bb8031c3036" /> : null}
+        <NextScript/>
       </body>
     </Html>
   );
